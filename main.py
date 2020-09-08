662e923e7869f1c96c257dff856715b260f62a3d
Sachin Raja <sachinraja2349@gmail.com>
2020-09-07 21:52:52 -0700

    Minor fixes, optimization, new commands
    When shopping for a part you can react with arrow reactions to change part being viewed for up to 5 minutes.
    
    replacecolor command - Replace the background color of the trees using a hex code.
    
    daily command - Adds a number between 50-100 to balance that has a cooldown of one day.
    
    balance command - View balance of yourself or others.

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import never_sleep
 import base64
 from io import BytesIO
 import requests
+from random import randint
+import asyncio
 
 # load token
 load_dotenv()
@@ -30,27 +32,26 @@ parts_col = db['parts']
 
 class Tree():
 
-    def __init__(self, base, trunk, leaf_pattern):
+    def __init__(self, base, trunk, leaf_pattern, background_color=(0, 0, 255)):
         self.base = base
         self.trunk = trunk
         self.leaf_pattern = leaf_pattern
+        self.background_color = background_color
     
 class User():
-    def __init__(self, user_id, tree_1, tree_2, tree_3, balance=1000, inventory={'bases' : [], 'trunks' : [], 'leaf_patterns' : []}):
+    def __init__(self, user_id, trees, balance=200, inventory=[]):
         self.user_id = user_id
-        self.tree_1 = tree_1
-        self.tree_2 = tree_2
-        self.tree_3 = tree_3
+        self.trees = trees
         self.balance = balance
         self.inventory = inventory
 
 def create_tree_image(user, tree_num):
     # save with BytesIO and then paste to image
-    base_image = Image.open(BytesIO(user[f'tree_{tree_num}']['base']))
-    trunk_image = Image.open(BytesIO(user[f'tree_{tree_num}']['trunk']))
-    leaf_pattern_image = Image.open(BytesIO(user[f'tree_{tree_num}']['leaf_pattern']))
-    tree_image = Image.new("RGB", (15, 15), (0, 0, 255))
-    tree_image.paste(base_image, (0, 12), base_image)
+    base_image = Image.open(BytesIO(user['trees'][tree_num]['base']['part_info']['image']))
+    trunk_image = Image.open(BytesIO(user['trees'][tree_num]['trunk']['part_info']['image']))
+    leaf_pattern_image = Image.open(BytesIO(user['trees'][tree_num]['leaf_pattern']['part_info']['image']))
+    tree_image = Image.new("RGB", (15, 15), tuple(user['trees'][tree_num]['background_color']))
+    tree_image.paste(base_image, (0, 12), mask=base_image)
     tree_image.paste(trunk_image, mask=trunk_image)
     tree_image.paste(leaf_pattern_image, mask=leaf_pattern_image)
     
@@ -73,6 +74,16 @@ def binary_to_embed(binary):
         image_binary.seek(0)
         im_part = discord.File(fp=image_binary, filename='image.png')
         return im_part
+
+def create_tree_embed(user, author_name, input_tree_num, tree_num):
+    embed = discord.Embed(title=f'Tree {input_tree_num}', color=25600)\
+        .set_author(name=author_name)\
+        .add_field(name='Base', value=f"{user['trees'][tree_num]['base']['creator']}'s {user['trees'][tree_num]['base']['part_info']['name']}")\
+        .add_field(name='Trunk', value=f"{user['trees'][tree_num]['trunk']['creator']}'s {user['trees'][tree_num]['trunk']['part_info']['name']}")\
+        .add_field(name='Leaf Pattern', value=f"{user['trees'][tree_num]['leaf_pattern']['creator']}'s {user['trees'][tree_num]['leaf_pattern']['part_info']['name']}")\
+        .set_image(url='attachment://image.png')
+
+    return embed
 
 async def check_attachment(ctx, width, height):
     # check for attachment
@@ -109,65 +120,65 @@ default_leaf_pattern = None
 with open("default_leaf_pattern.png", "rb") as imageFile:
     default_leaf_pattern = imageFile.read()
 
-default_tree = Tree(default_base, default_trunk, default_leaf_pattern)
+default_tree = Tree({'part_info' : {'image' : default_base, 'name' : 'Default Base', 'type' : 'base', 'price' : 0}, 'creator' : 'Default'}, {'part_info' : {'image' : default_trunk, 'name' : 'Default Trunk', 'type' : 'trunk', 'price' : 0}, 'creator' : 'Default'}, {'part_info' : {'image' : default_leaf_pattern, 'name' : 'Default Leaf Pattern', 'type' : 'leafpattern', 'price' : 0}, 'creator' : 'Default'})
+default_trees = (default_tree.__dict__, default_tree.__dict__, default_tree.__dict__)
 
-code_matchings = {'base' : 'bases', 'trunk' : 'trunks', 'leafpattern' : 'leaf_patterns'}
+valid_parts = ('base', 'trunk', 'leafpattern')
+code_matchings = {'base' : 'base', 'trunk' : 'trunk', 'leafpattern' : 'leaf_pattern'}
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
 @bot.command(name='reset')
-async def reset_tree(ctx, tree_num : int):
+async def reset_tree(ctx, input_tree_num : int):
 
     # check for valid tree
-    if tree_num < 1 or tree_num > 3:
+    if input_tree_num < 1 or input_tree_num > 3:
         await ctx.send('Enter a number from 1 to 3.')
         return
+
+    tree_num = input_tree_num - 1
 
     user = tree_col.find_one({'user_id' : ctx.author.id})
     
     if user == None:
-        user = User(ctx.author.id, default_tree.__dict__, default_tree.__dict__, default_tree.__dict__).__dict__
+        user = User(ctx.author.id, default_trees).__dict__
         tree_col.insert_one(user)
 
     # Reset tree and update in db
-    user[f'tree_{tree_num}'] = default_tree.__dict__
+    user['trees'][tree_num] = default_tree.__dict__
     tree_col.update_one({'user_id' : ctx.author.id}, {"$set" : user})
 
     im_tree = create_tree_image(user, tree_num)
-    
-    embed = discord.Embed(title='Tree', color=25600)\
-        .set_author(name=ctx.author.name)\
-        .set_image(url='attachment://image.png')
+    embed = create_tree_embed(user, ctx.author.name, input_tree_num, tree_num)
     
     await ctx.send(file=im_tree, embed=embed)
 
 @bot.command(name='tree')
-async def display_tree(ctx, tree_num : int):
+async def display_tree(ctx, input_tree_num : int):
 
-    if tree_num < 1 or tree_num > 3:
+    if input_tree_num < 1 or input_tree_num > 3:
         await ctx.send('Enter a number from 1 to 3.')
         return
+
+    tree_num = input_tree_num - 1
 
     user = tree_col.find_one({'user_id' : ctx.author.id})
     
     if user == None:
-        user = User(ctx.author.id, default_tree.__dict__, default_tree.__dict__, default_tree.__dict__).__dict__
+        user = User(ctx.author.id, default_trees).__dict__
         tree_col.insert_one(user)
 
     im_tree = create_tree_image(user, tree_num)
-
-    embed = discord.Embed(title=f'Tree {tree_num}', color=25600)\
-        .set_author(name=ctx.author.name)\
-        .set_image(url='attachment://image.png')
+    embed = create_tree_embed(user, ctx.author.name, input_tree_num, tree_num)
 
     await ctx.send(file=im_tree, embed=embed)
     
 @bot.command(name="list")
 async def upload(ctx, part_type, part_name, list_price : int):
     
-    if part_type not in ['base', 'trunk', 'leafpattern']:
+    if part_type not in valid_parts:
         await ctx.send("Enter base, trunk, or leafpattern.")
         return
         
@@ -186,24 +197,27 @@ async def upload(ctx, part_type, part_name, list_price : int):
     user = parts_col.find_one({'user_id' : ctx.author.id})
     
     if user == None:
-        user = {'user_id' : ctx.author.id, 'bases' : [], 'trunks' : [], 'leaf_patterns' : []}
+        user = {'user_id' : ctx.author.id, 'parts' : []}
         parts_col.insert_one(user)
 
-    code_part_type = code_matchings[part_type]
+    # prepare for future where they can gain money
+    if tree_col.find_one({'user_id' : ctx.author.id}) == None:
+        tree_col.insert_one(User(ctx.author.id, default_trees).__dict__)
 
-    parts = user[code_part_type]
-    if len(parts) >= 3:
-        await ctx.send(f"You have exceeded your limit of 3 on {part_type}s.")
+    parts = user['parts']
+
+    if len(parts) >= 5:
+        await ctx.send(f"You have exceeded your limit of 5 parts.")
         return
 
     for part in parts:
         if part['name'].lower() == part_name.lower():
-            await ctx.send(f"You have already listed a {part_type} with the name {parts['name']}.")
+            await ctx.send(f"You have already listed a part with the name {part['name']}.")
             return
 
     attachment_url = ctx.message.attachments[0].url
     file_request = requests.get(attachment_url)
-    parts.append({'image' : file_request.content, 'name' : part_name, 'price' : list_price})
+    parts.append({'image' : file_request.content, 'name' : part_name, 'type' : part_type, 'price' : list_price})
 
     parts_col.update_one({'user_id' : ctx.author.id}, {'$set' : user})
 
@@ -218,12 +232,15 @@ async def upload(ctx, part_type, part_name, list_price : int):
     await ctx.send(file=im_trunk, embed=embed)
 
 @bot.command(name="shop")
-async def shop_parts(ctx, part_type, member : discord.Member):
-
-    if part_type not in ['base', 'trunk', 'leafpattern']:
+async def shop_parts(ctx, part_type, member : discord.Member = None):
+    
+    if part_type not in valid_parts:
         await ctx.send("Enter base, trunk, or leafpattern.")
         return
 
+    if member == None:
+        member = ctx.author
+    
     seller = parts_col.find_one({'user_id' : member.id})
     
     if seller == None:
@@ -231,62 +248,238 @@ async def shop_parts(ctx, part_type, member : discord.Member):
         return
     
     i = 0
-    code_part_type = code_matchings[part_type]
-    parts = seller[code_part_type]
-    for part in parts:
-        part_picture = binary_to_embed(parts[i]['image'])
 
-        embed = discord.Embed(title=f'{part_type} Listing {i + 1}', color=255)\
-            .set_author(name=member.name)\
-            .add_field(name='Name', value=part['name'])\
-            .add_field(name='List Price', value=part['price'])\
-            .set_image(url='attachment://image.png')
-
-        await ctx.send(file=part_picture, embed=embed)
-
-        i += 1
-
-@bot.command(name="buy")
-async def buy_part(ctx, part_type, part_name, member : discord.Member):
-
-    if part_type not in ['base', 'trunk', 'leafpattern']:
-        await ctx.send("Enter base, trunk, or leafpattern.")
+    all_parts = seller['parts']
+    
+    parts = []
+    for p in all_parts:
+        if p['type'] == part_type:
+            parts.append(p)
+    
+    if parts == []:
+        await ctx.send(f"{seller} has no {part_type}s.")
         return
+
+    part_picture = binary_to_embed(parts[0]['image'])
+
+    embed = discord.Embed(title=f"{parts[0]['type']} Listing 1", color=255)\
+        .set_author(name=member.name)\
+        .add_field(name='Name', value=parts[0]['name'])\
+        .add_field(name='List Price', value=parts[0]['price'])\
+        .set_image(url='attachment://image.png')
+
+    shop_message = await ctx.send(file=part_picture, embed=embed)
+
+    await shop_message.add_reaction('⬅️')
+    await shop_message.add_reaction('➡️')
+
+    def check(reaction, user):
+        return reaction.message.id == shop_message.id and user.id == ctx.author.id and str(reaction.emoji) in ['⬅️', '➡️']
+
+    total_parts = len(parts) - 1
+    current_part = 0
+
+    while True:
+        try:
+            reaction, user = await bot.wait_for('reaction_add', check=check, timeout=300)
+
+            # check for arrow reactions
+            if str(reaction.emoji) == '⬅️' and current_part != 0:
+                current_part -= 1
+
+                part_picture = binary_to_embed(parts[current_part]['image'])
+
+                embed = discord.Embed(title=f"{parts[current_part]['type']} Listing {current_part + 1}", color=255)\
+                    .set_author(name=member.name)\
+                    .add_field(name='Name', value=parts[current_part]['name'])\
+                    .add_field(name='List Price', value=parts[current_part]['price'])\
+                    .set_image(url='attachment://image.png')
+
+                await shop_message.edit(embed=embed)
+                await shop_message.remove_reaction(reaction, user)
+
+            elif str(reaction.emoji) == '➡️' and current_part != total_parts:
+                current_part += 1
+
+                part_picture = binary_to_embed(parts[current_part]['image'])
+
+                embed = discord.Embed(title=f"{parts[current_part]['type']} Listing {current_part + 1}", color=255)\
+                    .set_author(name=member.name)\
+                    .add_field(name='Name', value=parts[current_part]['name'])\
+                    .add_field(name='List Price', value=parts[current_part]['price'])\
+                    .set_image(url='attachment://image.png')
+
+                await shop_message.edit(embed=embed)
+                await shop_message.remove_reaction(reaction, user)
+
+        except asyncio.TimeoutError:
+            break
+    
+@bot.command(name="buy")
+async def buy_part(ctx, part_name, member : discord.Member):
     
     user = tree_col.find_one({'user_id' : ctx.author.id})
     
     if user == None:
-        user = User(ctx.author.id, default_tree.__dict__, default_tree.__dict__, default_tree.__dict__).__dict__
+        user = User(ctx.author.id, default_trees).__dict__
         tree_col.insert_one(user)
 
     seller = parts_col.find_one({'user_id' : member.id})
-    
+
     if seller == None:
         await ctx.send(f"{member} has nothing in their shop.")
         return
 
     # get correct spelling of list in seller
-    code_part_type = code_matchings[part_type]
-
-    parts = seller[code_part_type]
+    all_parts = seller['parts']
     part_to_buy = None
 
-    for part in parts:
+    for part in all_parts:
         if part['name'].lower() == part_name.lower():
             part_to_buy = part
     
     if part_to_buy == None:
-        await ctx.send(f"{member} doesn't have a {part_type} named {part_name}.")
+        await ctx.send(f"{member} doesn't have a part named {part_name}.")
         return
     
     if part_to_buy['price'] > user['balance']:
-        await ctx.send(f"{ctx.author} only has {user['balance']}, but this costs {part_to_buy['price']}.")
+        await ctx.send(f"You only have ${user['balance']}, but {part_to_buy['name']} costs ${part_to_buy['price']}.")
         return
     
-    user['inventory'][code_part_type].append(part_to_buy)
+    # give part and remove money from user
+    user['inventory'].append({'part_info' : part_to_buy, 'creator' : str(member)})
     user['balance'] -= part_to_buy['price']
     tree_col.update_one({'user_id' : ctx.author.id}, {'$set' : user})
 
-    await ctx.send(f"{ctx.author} bought {member}'s {part_type}, {part_to_buy['name']}.")
+    # give money to seller
+    seller_tree_account = tree_col.find_one({'user_id' : member.id})
+    seller_tree_account['balance'] += part_to_buy['price']
+    tree_col.update_one({'user_id' : member.id}, {'$set' : seller_tree_account})
+
+    await ctx.send(f"You bought {member}'s {part_to_buy['name']}.")
+
+@bot.command(name="inventory")
+async def list_inventory(ctx, member : discord.Member = None):
+
+    if member == None:
+        member = ctx.author
+    
+    user = tree_col.find_one({'user_id' : member.id})
+
+    embed = discord.Embed(title=f"{member}'s Inventory", color=255)
+    
+    i = 1
+    for part in user['inventory']:
+        embed = embed.add_field(name=part['part_info']['type'], value=f"{i} : {part['creator']}'s {part['part_info']['name']}", inline=False)
+
+        i += 1
+
+    await ctx.send(embed=embed)
+
+@bot.command(name="replace")
+async def replace_part(ctx, input_tree_num : int, input_inventory_num : int):
+    if input_tree_num < 1 or input_tree_num > 3:
+        await ctx.send('Enter a number from 1 to 3.')
+        return
+
+    tree_num = input_tree_num - 1
+
+    user = tree_col.find_one({'user_id' : ctx.author.id})
+    
+    if user == None:
+        await ctx.send(f'There is no part at inventory #{input_inventory_num}.')
+        return
+    
+    inventory_num = input_inventory_num - 1
+
+    # check for proper inventory number
+    if inventory_num <= 0:
+        await ctx.send(f'Inventory numbers must be over 0.')
+        return
+    
+    elif len(user['inventory']) - 1 < inventory_num:
+        await ctx.send(f"Your inventory only goes up to {len(user['inventory'])}, but you entered #{input_inventory_num}.")
+        return
+    
+    # get new part from user
+    part = user['inventory'].pop(inventory_num)
+    
+    # set tree part to new part
+    part_type = code_matchings[part['part_info']['type']]
+    user['trees'][tree_num][part_type] = part
+
+    tree_col.update_one({'user_id' : ctx.author.id}, {'$set' : user})
+
+    im_tree = create_tree_image(user, tree_num)
+    embed = create_tree_embed(user, ctx.author.name, input_tree_num, tree_num)
+
+    await ctx.send(file=im_tree, embed=embed)
+
+@bot.command(name="replacecolor")
+async def replace_color(ctx, input_tree_num : int, hex_code):
+    if input_tree_num < 1 or input_tree_num > 3:
+        await ctx.send('Enter a number from 1 to 3.')
+        return
+
+    tree_num = input_tree_num - 1
+
+    if len(hex_code) != 7:
+        await ctx.send('Enter a valid hex code. Ex: #0000FF.')
+        return
+    
+    user = tree_col.find_one({'user_id' : ctx.author.id})
+    
+    if user == None:
+        user = User(ctx.author.id, default_trees).__dict__
+        tree_col.insert_one(user)
+    
+    # convert to rgb
+    h = hex_code.lstrip('#')
+    user['trees'][tree_num]['background_color'] = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+    tree_col.update_one({'user_id' : ctx.author.id}, {'$set' : user})
+
+    im_tree = create_tree_image(user, tree_num)
+    embed = create_tree_embed(user, ctx.author.name, input_tree_num, tree_num)
+    
+    await ctx.send(file=im_tree, embed=embed)
+
+@bot.command(name="daily")
+@commands.cooldown(1, 60*60*24, commands.BucketType.user)
+async def daily_reward(ctx):
+
+    user = tree_col.find_one({'user_id' : ctx.author.id})
+    
+    if user == None:
+        user = User(ctx.author.id, default_trees).__dict__
+        tree_col.insert_one(user)
+    
+    reward = randint(50, 100)
+    user['balance'] += randint(50, 100)
+
+    tree_col.update_one({'user_id' : ctx.author.id}, {'$set' : user})
+
+    await ctx.send(f"Added ${reward} to your balance! You now have ${user['balance']}.")
+
+@daily_reward.error
+async def daily_reward_error(ctx, error):
+    # error message if the command is on cooldown still
+    if isinstance(error, commands.CommandOnCooldown):
+        # time left on cooldown converted from seconds to hours
+        await ctx.send(f'This command is on cooldown, try again in{error.retry_after / 3600: .1f} hours.')
+
+@bot.command(name="balance")
+async def check_balance(ctx, member : discord.Member = None):
+
+    if member == None:
+        member = ctx.author
+
+    user = tree_col.find_one({'user_id' : member.id})
+    
+    if user == None:
+        user = User(ctx.author.id, default_trees).__dict__
+        tree_col.insert_one(user)
+    
+    await ctx.send(f"{member} has ${user['balance']}.")
+
 # never_sleep.awake('https://Bonsai-Discord-Bot.xcloudzx.repl.co', False)
 bot.run(TOKEN)
