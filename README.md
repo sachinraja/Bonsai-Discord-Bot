0b2249ed7a4496382644b08dc73c6edb515beb42
Sachin Raja <sachinraja2349@gmail.com>
2020-10-04 22:13:59 -0700

    Update prefix information

--- README.md
+++ README.md
@@ -19,23 +19,26 @@
 <p>Part Type: base, trunk, or leafpattern.</p>
 <p>Member=optional: If you don't input a member here, it will default to yourself.</p>
 <hr width="50%" align="left">
+<h3>Prefix</h3>
+<p>The default prefix is b!. This can be changed with the prefix command. Commands can also be called by mentioning the bot.</p>
+<hr width="50%" align="left">
 <h3>Commands</h3>
-<p>b!about: Displays a general description of the bot as well as other useful information about it.<p>
-<p>b!balance [Member=optional]: Shows your balance or the balance of someone else.<p>
-<p>b!buy [Part Name] [Member=optional]: Buy a part from Member.</p>
-<p>b!color [Color Hex Code] [Tree Name]: Replace the background color of a tree.</p>
-<p>b!create [Tree Name <= 50 characters]: Creates a tree with the name Tree Name.</p>
-<p>b!daily: Claim your daily reward ($50-$100).</p>
-<p>b!help: Displays the help message with the list of commands.</p>
-<p>b!inventory [Member=optional]: Displays all the parts in your inventory and their corresponding inventory number.</p>
-<p>b!list [Part Type] [Part Name <= 50 characters] [0 < List Price < 10000]: List a part for sale. This must have an attached image to use for the part. Bases must be 15 x 3. Trunks must be 15 x 12. Leaf Patterns must be 15 x 12.</p>
-<p>b!prefix [New Prefixes]: Change the bot's prefixes on a server. Separate new prefixes with a space.</p>
-<p>b!removeinventory [Inventory Number]: Remove a part from your inventory (does not refund any money).</p>
-<p>b!replace [Inventory Number] [Tree Name]: Replace a part on a tree with one in your inventory.</p>
-<p>b!reset [Tree Name]: Reset a tree to defaults.</p>
-<p>b!shop [Part Type] [Member=optional]: Show all the parts that Member has listed as Part Type.</p>
-<p>b!top: Shows the users with the top ten balances.</p>
-<p>b!tree [Tree Name]: Displays the tree.</p>
-<p>b!trees [Member=optional]: Displays all of Member's trees.</p>
-<p>b!unlist [Part Name]: Remove a part listed in your shop.</p>
-<p>b!visit [Member] [Tree Name]: Display Member's tree Tree Name.</p>
+<p>about: Displays a general description of the bot as well as other useful information about it.<p>
+<p>balance [Member=optional]: Shows your balance or the balance of someone else.<p>
+<p>buy [Part Name] [Member=optional]: Buy a part from Member.</p>
+<p>color [Color Hex Code] [Tree Name]: Replace the background color of a tree.</p>
+<p>create [Tree Name <= 50 characters]: Creates a tree with the name Tree Name.</p>
+<p>daily: Claim your daily reward ($50-$100).</p>
+<p>help: Displays the help message with the list of commands.</p>
+<p>inventory [Member=optional]: Displays all the parts in your inventory and their corresponding inventory number.</p>
+<p>list [Part Type] [Part Name <= 50 characters] [0 < List Price < 10000]: List a part for sale. This must have an attached image to use for the part. Bases must be 15 x 3. Trunks must be 15 x 12. Leaf Patterns must be 15 x 12.</p>
+<p>prefix [New Prefixes]: Change the bot's prefixes on a server. Separate new prefixes with a space.</p>
+<p>removeinventory [Inventory Number]: Remove a part from your inventory (does not refund any money).</p>
+<p>replace [Inventory Number] [Tree Name]: Replace a part on a tree with one in your inventory.</p>
+<p>reset [Tree Name]: Reset a tree to defaults.</p>
+<p>shop [Part Type] [Member=optional]: Show all the parts that Member has listed as Part Type.</p>
+<p>top: Shows the users with the top ten balances.</p>
+<p>tree [Tree Name]: Displays the tree.</p>
+<p>trees [Member=optional]: Displays all of Member's trees.</p>
+<p>unlist [Part Name]: Remove a part listed in your shop.</p>
+<p>visit [Member] [Tree Name]: Display Member's tree Tree Name.</p>
--- bot.py
+++ bot.py
@@ -51,25 +51,26 @@ async def help_message(ctx):
 
     embed = discord.Embed(title="Help - List of Commands", color=65280)\
         .add_field(name="Argument Definitions", value="Part Type : base, trunk, or leaves.\nMember=optional : If you do not input a member here, it will default to yourself.")\
-        .add_field(name="b!about", value="Displays a general description of the bot as well as other useful information about it.", inline=False)\
-        .add_field(name="b!balance [Member=optional]", value="Shows your balance or the balance of someone else.", inline=False)\
-        .add_field(name="b!buy [Part Name] [Member=optional]", value="Buy a part from Member.", inline=False)\
-        .add_field(name="b!color [Color Hex Code] [Tree Name]", value="Replace the background color of a tree.", inline=False)\
-        .add_field(name="b!create [Tree Name <= 50 characters]", value="Creates a tree with the name Tree Name.", inline=False)\
-        .add_field(name="b!daily", value="Claim your daily reward ($50-$100).", inline=False)\
-        .add_field(name="b!help", value="Displays this message.", inline=False)\
-        .add_field(name="b!inventory [Member=optional]", value="Displays all the parts in the inventory and their corresponding inventory number.", inline=False)\
-        .add_field(name="b!list [Part Type] [Part Name <= 50 characters] [0 < List Price < 10000]", value="List a part for sale. This must have an attached image to use for the part. Bases must be 15 x 3. Trunks must be 15 x 12. Leaves must be 15 x 12.", inline=False)\
-        .add_field(name="b!prefix [New Prefixes]", value="Change the bot's prefixes on your server. Separate new prefixes with a space.", inline=False)\
-        .add_field(name="b!removeinventory [Inventory Number]", value="Remove a part from your inventory (does not refund any money).", inline=False)\
-        .add_field(name="b!replace [Inventory Number] [Tree Name]", value="Replace a part on a tree with one in your inventory.", inline=False)\
-        .add_field(name="b!reset [Tree Name]", value="Reset a tree to defaults.", inline=False)\
-        .add_field(name="b!shop [Part Type] [Member=optional]", value="Show all the parts that Member has listed as Part Type.", inline=False)\
-        .add_field(name="b!top", value="Shows the users with the top ten balances.", inline=False)\
-        .add_field(name="b!tree [Tree Name]", value="Displays the tree Tree Name.", inline=False)\
-        .add_field(name="b!trees [Member=optional]", value="Displays all of Member's trees.", inline=False)\
-        .add_field(name="b!unlist [Part Name]", value="Remove a part listed in your shop.", inline=False)\
-        .add_field(name="b!visit [Member] [Tree Name]", value="Display Member's tree Tree Name.")
+        .add_field(name="Prefix", value="The default prefix is b!. You can change this with the prefix command. You can also call commands by mentioning the bot.")\
+        .add_field(name="about", value="Displays a general description of the bot as well as other useful information about it.", inline=False)\
+        .add_field(name="balance [Member=optional]", value="Shows your balance or the balance of someone else.", inline=False)\
+        .add_field(name="buy [Part Name] [Member=optional]", value="Buy a part from Member.", inline=False)\
+        .add_field(name="color [Color Hex Code] [Tree Name]", value="Replace the background color of a tree.", inline=False)\
+        .add_field(name="create [Tree Name <= 50 characters]", value="Creates a tree with the name Tree Name.", inline=False)\
+        .add_field(name="daily", value="Claim your daily reward ($50-$100).", inline=False)\
+        .add_field(name="help", value="Displays this message.", inline=False)\
+        .add_field(name="inventory [Member=optional]", value="Displays all the parts in the inventory and their corresponding inventory number.", inline=False)\
+        .add_field(name="list [Part Type] [Part Name <= 50 characters] [0 < List Price < 10000]", value="List a part for sale. This must have an attached image to use for the part. Bases must be 15 x 3. Trunks must be 15 x 12. Leaves must be 15 x 12.", inline=False)\
+        .add_field(name="prefix [New Prefixes]", value="Change the bot's prefixes on your server. Separate new prefixes with a space.", inline=False)\
+        .add_field(name="removeinventory [Inventory Number]", value="Remove a part from your inventory (does not refund any money).", inline=False)\
+        .add_field(name="replace [Inventory Number] [Tree Name]", value="Replace a part on a tree with one in your inventory.", inline=False)\
+        .add_field(name="reset [Tree Name]", value="Reset a tree to defaults.", inline=False)\
+        .add_field(name="shop [Part Type] [Member=optional]", value="Show all the parts that Member has listed as Part Type.", inline=False)\
+        .add_field(name="top", value="Shows the users with the top ten balances.", inline=False)\
+        .add_field(name="tree [Tree Name]", value="Displays the tree Tree Name.", inline=False)\
+        .add_field(name="trees [Member=optional]", value="Displays all of Member's trees.", inline=False)\
+        .add_field(name="unlist [Part Name]", value="Remove a part listed in your shop.", inline=False)\
+        .add_field(name="visit [Member] [Tree Name]", value="Display Member's tree Tree Name.")
 
     await ctx.send(embed=embed)
 
